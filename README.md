00e0de9748f4b7c0e78d259910af4074d445b86b
Muhammad <muhamadshehroz10089@icloud.com>
2022-07-04 15:18:53 -0400

    added the server.js and radme file

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # Note Taker Starter Code
+
+In this project i created a server.js file. i went ahead by first creating a route to homepage. Then i created routes for the page where i can add notes. Then i used node random id package to add ids to every new object that gtes added to the array.Then i created a post mehtod, in that method i gave every whatevers inside the body a uniuqe id by using the npm package. To add data first i used readfile to read the json file wehre its going to be added. and set it to a variable. Then converted that into json file using parse and set it to another object. Created a variable and set the incoming content to that. Since now it has been converted to into an array object used .push to push the new content to the json file. Then stringlified that data and used write file to add it to the json file.
+
+# Pictures
+<img width="1440" alt="Screen Shot 2022-07-04 at 3 17 03 PM" src="https://user-images.githubusercontent.com/97645096/177208080-ca93be5d-ae6a-49e2-b547-131a2fe243cf.png">
+<img width="1440" alt="Screen Shot 2022-07-04 at 3 16 45 PM" src="https://user-images.githubusercontent.com/97645096/177208082-2ed9e6a6-5bb0-440a-b64a-575801875c5b.png">
+<img width="1440" alt="Screen Shot 2022-07-04 at 3 16 41 PM" src="https://user-images.githubusercontent.com/97645096/177208083-ec54bde7-2037-4065-b12f-4dd3fcc3bdca.png">
+<img width="1440" alt="Screen Shot 2022-07-04 at 3 17 07 PM" src="https://user-images.githubusercontent.com/97645096/177208086-c1bae892-80bd-4759-960e-fef3b644ee42.png">
--- server.js
+++ server.js
@@ -1,59 +1,62 @@
+//using the unique id node package
 const { v4: uuidv4 } = require('uuid');
+//using file system to read and write files in this orogram
 const fs = require('fs');
+//setting up express 
 const express = require('express');
+// setting the instance of express to app
 const app = express();
+//using path to join files 
 const path = require('path');
+// setting the port
 const PORT = process.env.PORT || 3001;
-
+//getting the json file and setting it to a data
 const data = require('./db/db.json');
-const { json } = require('express');
-
+//using middleware to making all files public so css and html and link
 app.use(express.static('public'));
 app.use(express.json());
-app.use(express.urlencoded({extended:true}));
+//allows to choose between parsing the URL-encoded data with the querystring library (when false) or the qs library (when true). The "extended" syntax allows for rich objects and arrays to be encoded into the URL-encoded format, allowing for a JSON-like experience with URL-encoded.
+app.use(express.urlencoded({ extended: true }));
 
-
-app.post('/api/notes',(req, res)=>{
+//setting a post request
+app.post('/api/notes', (req, res) => {
+  //giving every object a new unique id
   req.body.id = uuidv4();
-  // fs.readFile('./db/db.json', (err, results)=>{
-  //   if(err){
-  //     throw err;
-  // }
-  var jsonData = fs.readFile('./db/db.json');
+  //reading the json file
+  var jsonData = fs.readFileSync('./db/db.json');
+  // parsing the json data
   var myObject = JSON.parse(jsonData);
-
-    // const finalResults = JSON.parse(results);
-    let newNote = req.body;
-    myObject.push(newNote);
-    var data2 = json.stringify(myObject);
-
-    // var newData = JSON.stringify(finalResults);
-    fs.writeFile('./db/db.json',data2,(err)=>{
-      if(err)
+  // setting the incoming conent to neeNote
+  let newNote = req.body;
+  //pushing incoming conent to myObject
+  myObject.push(newNote);
+  //stringlifying the data
+  var data2 = JSON.stringify(myObject);
+  //using write file to add data and setting err method
+  fs.writeFileSync('./db/db.json', data2, (err) => {
+    if (err)
       throw err
-    } )
-    // console.log(newData);
   })
+})
 
-
-
-app.get('/api/notes',(req, res)=>{
-  fs.readFile('./db/db.json', (err, results)=>{
-    if(err){
+//using get request and reading the file and sending the results
+app.get('/api/notes', (req, res) => {
+  fs.readFile('./db/db.json', (err, results) => {
+    if (err) {
       throw err;
     }
     res.send(results);
   })
 })
-
+//using  aget request and sending the files using Path to join all the linked files
 app.get('/notes', (req, res) => {
   res.sendFile(path.join(__dirname, './public/notes.html'));
 });
-
+//setting a default page
 app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, './public/index.html'));
-  });
-
+  res.sendFile(path.join(__dirname, './public/index.html'));
+});
+//checking to see if we are on right port and server is working
 app.listen(PORT, () => {
-    console.log(`API server now on port ${PORT}!`);
-  });
+  console.log(`API server now on port ${PORT}!`);
+});
